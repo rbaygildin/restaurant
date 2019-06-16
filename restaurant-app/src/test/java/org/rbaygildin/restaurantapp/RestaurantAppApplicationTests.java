cf7b4c60b404ef7638a62390b2d7600145e67ad2
Roman Baygildin <rbaygildin95@gmail.com>
2019-06-16 16:20:53 +0300

    SPRING-DEMO [DEV] - Init commit

---
+++ restaurant-app/src/test/java/org/rbaygildin/restaurantapp/RestaurantAppApplicationTests.java
@@ -0,0 +1,16 @@
+package org.rbaygildin.restaurantapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestaurantAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
